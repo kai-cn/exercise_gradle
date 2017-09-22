2936c768cdea05cd4f6de2db78ca96c1c5576b96
kklin <kai.lin@szjlxh.com>
2017-09-22 20:53:13 +0800

    完成了Java Console gradle project的创建

---
+++ to-do-java/src/main/java/kklin/todo/ToDoApp.java
@@ -0,0 +1,24 @@
+package kklin.todo;
+
+import kklin.todo.utils.CommandLineInput;
+import kklin.todo.utils.CommandLineInputHandler;
+
+import org.apache.commons.lang3.CharUtils;
+
+public class ToDoApp {
+    public static final char DEFAULT_INPUT = '\u0000';
+
+    public static void main(String args[]) {
+        CommandLineInputHandler commandLineInputHandler = new CommandLineInputHandler();
+        char command = DEFAULT_INPUT;
+
+        while (CommandLineInput.EXIT.getShortCmd() != command) {
+            commandLineInputHandler.printOptions();
+            String input = commandLineInputHandler.readInput();
+            System.out.println("-----> " + CharUtils.toChar(input, DEFAULT_INPUT));
+            command = CharUtils.toChar(input, DEFAULT_INPUT);
+            CommandLineInput commandLineInput = CommandLineInput.getCommandLineInputForInput(command);
+            commandLineInputHandler.processInput(commandLineInput);
+        }
+    }
+}
